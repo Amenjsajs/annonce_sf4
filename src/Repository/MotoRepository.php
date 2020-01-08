37aee7d699232ce3b8b1ccb9a358613f0dffec47
Ange <32566503+Amenjsajs@users.noreply.github.com>
2020-01-08 14:08:35 +0000

    Update MotoRepository.php

--- src/Repository/MotoRepository.php
+++ src/Repository/MotoRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Moto;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MotoRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Moto::class);
     }
