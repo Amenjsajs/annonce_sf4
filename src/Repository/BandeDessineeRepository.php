b0751fa959f4b8954dea6a6b0c5836b3f863d4d1
Ange <32566503+Amenjsajs@users.noreply.github.com>
2020-01-08 13:59:55 +0000

    Update BandeDessineeRepository.php

--- src/Repository/BandeDessineeRepository.php
+++ src/Repository/BandeDessineeRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\BandeDessinee;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class BandeDessineeRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, BandeDessinee::class);
     }
