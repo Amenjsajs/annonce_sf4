af394bbea101c4621b4bcdda719c82b1cb444f2c
Ange <32566503+Amenjsajs@users.noreply.github.com>
2020-01-08 14:06:57 +0000

    Update MontreRepository.php

--- src/Repository/MontreRepository.php
+++ src/Repository/MontreRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Montre;
+use Doctrine\Persistence\ManagerRegistry
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -14,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MontreRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Montre::class);
     }
